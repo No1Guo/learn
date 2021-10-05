ad72a313149e8fe2c0c1b44765d3ed4261f8e72d
lueguo <13135305305@163.com>
2021-10-05 21:25:49 +0800

    channel group usage

--- src/main/java/me/lueguo/nettyChat/handler/ChatServerHandler.java
+++ src/main/java/me/lueguo/nettyChat/handler/ChatServerHandler.java
@@ -10,38 +10,20 @@ import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.CharsetUtil;
 import io.netty.util.concurrent.GlobalEventExecutor;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class ChatServerHandler extends ChannelInboundHandlerAdapter {
 
-    private static HashMap<Channel,Integer> chMap = new HashMap<>();
-    private static ArrayList<Channel> chArr = new ArrayList<>();
-
     private static ChannelGroup channelGroup =  new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
-
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         Channel currentCh = ctx.channel();
 
         String actMsg = "Client [" + currentCh.remoteAddress() + "] is online.";
-//        ByteBuf byteBuf = Unpooled.copiedBuffer(actMsg, CharsetUtil.UTF_8);
-//        channelGroup.add(currentCh);
-//        channelGroup.writeAndFlush(byteBuf);
-
-        chArr.add(currentCh);
-        System.out.println(actMsg);
         ByteBuf byteBuf = Unpooled.copiedBuffer(actMsg, CharsetUtil.UTF_8);
-
-
-
-        for (Channel ch: chArr
-             ) {
-            ch.writeAndFlush(byteBuf);
-        }
+        channelGroup.add(currentCh);
+        channelGroup.writeAndFlush(byteBuf);
     }
-
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
